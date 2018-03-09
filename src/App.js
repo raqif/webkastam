2d8bdc94e299e3c3132edff05aef164a9e62ba80
raqif <32008556+raqif@users.noreply.github.com>
2018-03-09 22:18:06 +0800

    init commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">
+          Persatuan Pegawai Kastam Marin Malaysia
+          </h1>
+        </header>
+
+        <nav className="App-nav">
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">PPKMM</a>
+            </li>
+            <li>
+              <a href="#">Download</a>
+            </li>
+            <li>
+              <a href="#">Media</a>
+            </li>
+            <li>
+              <a href="#">Contact Us</a>
+            </li>          
+          </ul>
+        </nav>
+
+        <div className="App-body">
+          <p className="slideshow">
+          <img src={logo} className="App-logo" alt="logo" />
+          slide
+          </p>
+          <p className="activity">
+          <img src={logo} className="App-logo" alt="logo" />
+          show
+          </p>
+        </div>
+
+        <div className="App-body2">
+          <p className="App-link">
+            JKDM | JPA | Jabatan Laut | Biro Angkasa | Cuepacs
+            | PEBERKAS | APMM | Polis Marin
+          </p>
+        </div>
+
+        <footer className="App-footer">
+        copyright 2018
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
